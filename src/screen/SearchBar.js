06ada887335df4358fff84739b9e247092552c1c
mbobas <mbobas@gmail.com>
2019-11-06 21:54:23 +0100

    SearchBarLookComplete

--- src/screen/SearchBar.js
+++ src/screen/SearchBar.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
 
 const SearchBar = () => {
     return(
-        <View style={styles.background}>
-            <Text>Search Bar</Text>
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput
+                style={styles.inputStyle} 
+                placeholder="Search"
+            
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15,
-
-
+        flexDirection: 'row',
+        marginTop: 10,
+    },
+    inputStyle: {
+        //borderColor: 'black',
+        ///borderWidth: 1,  
+        flex: 1,
+        fontSize: 18,
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15,
 
     }
 });
