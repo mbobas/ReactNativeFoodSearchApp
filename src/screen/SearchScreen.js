9f20526c614fa3a6dca295d83112c0193d5ef73c
mbobas <mbobas@gmail.com>
2019-11-07 21:32:41 +0100

    filterResultsByPrice and ResultList

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -3,11 +3,18 @@ import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
         const [term, setTerm] = useState('');
         const [searchApi, results, errorMessage] = useResults();
         
+        const filterResultsByPrice = (price) => {
+            // price === '$' || '$$' || '$$$'
+            return results.filter(result => {
+                return result.price === price;
+            });
+        }; 
 
     return(
         <View>
@@ -20,6 +27,10 @@ const SearchScreen = () => {
 
             {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null }    
             <Text>We have found {results.length} results</Text>
+
+            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier"/>
+            <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
 
         </View>
     );
