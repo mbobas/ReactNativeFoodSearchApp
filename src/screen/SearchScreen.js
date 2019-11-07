11abb80c92b48e594bf478ed774d8bdc79ccaf89
mbobas <mbobas@gmail.com>
2019-11-07 20:48:09 +0100

    SearAPI catchError useEffect to startOneceSearch

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -6,24 +6,55 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
         const [term, setTerm] = useState('');
         const [results, setResults] = useState([]);
+        const [errorMessage, setErrorMessage] = useState('');
+
         
+        const searchApi = async (searchTerm) => {
+            console.log('testsearching test');
+            try {
+            const response = await yelp.get('/search', {
+                params: {
+                   limit: 50,
+                   term: searchTerm,
+                   location: 'san jose',
+
+                }
+            });
+            setResults(response.data.businesses);
+            }catch (err) {
+                setErrorMessage('Something went wrong');
+            }
+        };
+        //call searchAPI hwen component is first rendered. BAD CODE!!!!!
+        // searchApi('pasta); - is maaked a loop here....start agian..start agiain....
+
+        //RIGHT WAY TO SEARCH 1 time on start app
+        useEffect(()=> {
+            searchApi('pasta');         
+        }, []);
+
+        
+
     return(
         <View>
             <SearchBar 
                 term={term} 
-                onTermChange={(newTerm) => setTerm(newTerm)}
-                onTermSubbmit={() => console.log('term was submitted')}
+                onTermChange={setTerm}
+                onTermSubbmit={() => searchApi(term)}
 
                 />
 
-                
-            <Text>{term}</Text>
+            {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null }    
+            <Text>We have found {results.length} results</Text>
+
         </View>
     );
 };
 
 styles = StyleSheet.create({
-
+    error: {
+        color: 'red'
+    }
 });
 
 export default SearchScreen;
