c39f29480876a3d8c70b6a046a11c8638164cf82
mbobas <mbobas@gmail.com>
2019-11-27 20:18:52 +0100

    styling_marginAdded

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -5,7 +5,7 @@ import ResultsDetails from '../components/ResultsDetails';
 
 const ResultsList = ( {title, results} ) => {
     return(
-        <View>
+        <View style={styles.container}>
             <Text style={styles.title}> {title}</Text>
             <FlatList
                 horizontal={true}
@@ -24,7 +24,12 @@ const ResultsList = ( {title, results} ) => {
 const styles = StyleSheet.create({
     title: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5,
+    },
+    container: {
+        marginBottom: 10,
     }
 });
 
