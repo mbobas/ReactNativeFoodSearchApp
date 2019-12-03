94b3a138165631e88cede89c52f244f59a1f9ffa
mbobas <mbobas@gmail.com>
2019-12-03 19:30:35 +0100

    ResultShowSCreen_Images_added

--- src/screen/ResultShowScreen.js
+++ src/screen/ResultShowScreen.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
+
+
 
 const ResultShowScreen = ( {navigation} ) => {
+    const [ result, setResult ] = useState(null);
     const id = navigation.getParam('id');
-    console.log(id);
+    
+    //console.log(id);
+    //console.log(result);
+
+    const getResult = async id => {
+       const response = await yelp.get(`/${id}`);
+       //response.data;
+        setResult(response.data);
+    };
+    useEffect(() => {
+        getResult(id);
+    }, [] );
+
+    if (!result) {
+        return null;
+
+    }
+
 
     return <View>
-        <Text>Result Show</Text>
-    </View>
+            <Text>{result.name}</Text>
+            <FlatList
+                data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={({item}) => {
+                    return <Image style={styles.image} source={{ uri: item }} />
+                }}
+                />
+        </View>
 };
 
 const styles = StyleSheet.create({
-
+    image: {
+        height: 200,
+        width: 300,
+    }
 });
 
 
